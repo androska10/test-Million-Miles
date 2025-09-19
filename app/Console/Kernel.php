191ba552d85e50e0b89369005ce6821c534977c0
androska <polesuke10@gmail.com>
2025-09-19 07:03:08 +0300

    add Kernel minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('encar:parse')->everyFifteenMinutes();
+        $schedule->command('encar:parse')->everyMinutes();
     }
 
     /**
